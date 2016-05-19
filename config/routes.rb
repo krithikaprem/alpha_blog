c6777a47486f7dff6541051c486eb43377c88b38
krithika <krithika.1112@gmail.com>
2016-05-19 11:34:59 +0000

    Create new template

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root "alpha_blog#home"
   get "about",  to: "alpha_blog#about" 
-
+resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
