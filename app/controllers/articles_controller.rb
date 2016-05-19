c10a740b1d187216870a39a17cf353eac8673da4
krithika <krithika.1112@gmail.com>
2016-05-19 18:03:01 +0000

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,9 @@ class ArticlesController < ApplicationController
 def new
     @article = Article.new
 end
+def edit
+ @article = Article.find(params[:id])
+end
  def create 
      #render plain: params[:article].inspect
      @article = Article.new(article_params)
@@ -11,12 +14,21 @@ end
     else
      render 'new'
     end
-    def show
-    @article = Article.find(params[:id])
-    end    
  end
- private
+ def show
+ @article = Article.find(params[:id])
+ end   
+ def update 
+  @article = Article.find(params[:id])
+ if @article.update(article_params)
+ flash[:notice] = "Article was succefully updated"
+ redirect_to article_path(@article)
+else
+ render 'edit'
+ end
+ end
+end
+private
  def article_params
      params.require(:article).permit(:title,:description)  
  end
-end
