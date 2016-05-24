11d9f7ab4ef5cd1abe5280fda00a965ee3e108d0
krithika <krithika.1112@gmail.com>
2016-05-24 06:57:31 +0000

    Destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,9 +18,17 @@ end
      render 'new'
     end
  end
+ 
  def show
  @article = Article.find(params[:id])
- end   
+ end 
+ 
+ def destroy
+ @article = Article.find(params[:id])
+ @article.destroy
+ flash[:notice] = "Article was successfully deleted"
+ redirect_to articles_path
+ end
  def update 
   @article = Article.find(params[:id])
  if @article.update(article_params)
